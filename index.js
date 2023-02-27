fbec2ba01f6f18d9518ac14fd20daedce06b19d3
zahaanshapoorjee <zahaan@shapoorjee.com>
2023-02-27 07:47:45 +0530

    Added working post request implementation using Google Sheets API

--- index.js
+++ index.js
@@ -7,51 +7,49 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database={
-    players:[
-        {
-            name: 'Aryan Yadav',
-            price: '44M'
-        },
-        {
-            name: 'Zahaan Shapoorjee',
-            price: '39M'
-        }
-    ]
-}
-
-app.get('/test', (req,res)=>{
-    res.send('lol')
-})
+const spreadsheetId = '1E6iMfg7OmKf-39mIfpm6oGcGSogJAABad_bjTihh-Qg';
 
 app.get('/', async (req,res)=>{
-
     const auth = new google.auth.GoogleAuth({
         keyFile: 'credentials.json',
         scopes: 'https://www.googleapis.com/auth/spreadsheets'
     })
-
     const client = await auth.getClient();
-
     const googleSheets = google.sheets({version: 'v4', auth: client});
-
-    const spreadsheetId = '1E6iMfg7OmKf-39mIfpm6oGcGSogJAABad_bjTihh-Qg';
-
     const MetaData = await googleSheets.spreadsheets.get({
 
         auth,
         spreadsheetId
 
     })
-
     const getRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
         range: 'Players'
     })
-
     res.send(getRows.data);
 })
+
+app.post('/', async (req, res) =>{
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: 'credentials.json',
+        scopes: 'https://www.googleapis.com/auth/spreadsheets'
+    })
+    const client = await auth.getClient();
+    const googleSheets = google.sheets({version: 'v4', auth: client});
+    await googleSheets.spreadsheets.values.append({
+        spreadsheetId: spreadsheetId,
+        range: "PLAYERTESTSHEET",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          values: [[req.body.firstname, req.body.middlename, req.body.lastname, req.body.batch, req.body.phonenumber, req.body.gender, req.body.position1, req.body.position2, req.body.comments]],
+        },
+      });
+      res.send(req.body);
+
+
+} )
 
 app.listen(3001, (req,res)=>{
     console.log("Running on Port: 3001")
